65cc121b1b7dddc3568a40905b9d85d9681bd704
neverland <chenjiahan.jait@bytedance.com>
2025-10-18 18:46:44 +0800

    chore(deps): update Biome to v2 (#26)

--- playground/src/index.js
+++ playground/src/index.js
@@ -1,3 +1,3 @@
 export function double(a) {
-	return a * 2;
+  return a * 2;
 }
--- test/environments/index.test.ts
+++ test/environments/index.test.ts
@@ -4,30 +4,30 @@ import { expect, test } from '@playwright/test';
 import { createRsbuild, loadConfig } from '@rsbuild/core';
 
 test('should generate UMD and web bundle correctly', async ({ page }) => {
-	const rsbuild = await createRsbuild({
-		cwd: __dirname,
-		rsbuildConfig: (await loadConfig({ cwd: __dirname })).content,
-	});
+  const rsbuild = await createRsbuild({
+    cwd: __dirname,
+    rsbuildConfig: (await loadConfig({ cwd: __dirname })).content,
+  });
 
-	await rsbuild.build();
+  await rsbuild.build();
 
-	const { server, urls } = await rsbuild.preview();
+  const { server, urls } = await rsbuild.preview();
 
-	// Browser env
-	await page.goto(`${urls[0]}/umd`);
+  // Browser env
+  await page.goto(`${urls[0]}/umd`);
 
-	const test = page.locator('#test');
-	await expect(test).toHaveText('2');
+  const test = page.locator('#test');
+  await expect(test).toHaveText('2');
 
-	expect(existsSync(join(__dirname, 'dist/static/js/index.js'))).toBeTruthy();
+  expect(existsSync(join(__dirname, 'dist/static/js/index.js'))).toBeTruthy();
 
-	// Node.js env
-	writeFileSync(
-		join(__dirname, 'dist/package.json'),
-		JSON.stringify({ type: 'commonjs' }),
-	);
-	const { double } = require('./dist/umd/index.js');
-	expect(double(1)).toEqual(2);
+  // Node.js env
+  writeFileSync(
+    join(__dirname, 'dist/package.json'),
+    JSON.stringify({ type: 'commonjs' }),
+  );
+  const { double } = require('./dist/umd/index.js');
+  expect(double(1)).toEqual(2);
 
-	await server.close();
+  await server.close();
 });
